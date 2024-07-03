6abd6cda9f3af78acebe501194df01462ad1870f
MaheshiNav <maheshinawarathna29@gmail.com>
2024-07-03 11:04:22 -0700

    add path

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import cors from "cors";
 import connectDB from "./config/db";
 import userRoutes from "./routes/userRoutes";
 import draftRoutes from "./routes/draftsRoutes";
+import path from "path"; // Import path module
 
 dotenv.config();
 connectDB();
@@ -14,13 +15,13 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://wonderwriters.onrender.com"
+  "https://wonderwriters.onrender.com",
 ];
 
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin as string) !== -1 || !origin) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
@@ -32,11 +33,15 @@ app.use(
 app.use(bodyParser.json());
 
 app.use("/api/users", userRoutes);
-app.use("/api", draftRoutes); 
+app.use("/api", draftRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Server is up and running!");
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
-const PORT: string = process.env.PORT || '8080';
+const PORT: string = process.env.PORT || "8080";
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
